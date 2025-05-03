730e2ce550fd93bed345faa54d05abdf1d4f1f7b
yasinchenko <yasinchenko@gmail.com>
2025-05-03 13:02:24 +0300

    Ticket text was change

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -1,20 +1,19 @@
-// lib/core/theme.dart
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primaryColor = Color(0xFF4A90E2);
+  static ThemeData get theme => light;
 
-  static final ThemeData theme = ThemeData(
+  static final light = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-    scaffoldBackgroundColor: Colors.white,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.white,
-      foregroundColor: Colors.black,
-      elevation: 1,
-    ),
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: primaryColor,
-    ),
+    fontFamily: 'Roboto',
+  );
+
+  static final dark = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.blue,
+    useMaterial3: true,
+    fontFamily: 'Roboto',
   );
 }
